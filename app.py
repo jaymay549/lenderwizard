746a2ba053c53b567928a57e84b6eedc9febdfc1
jaymay549 <jason@bkd.ai>
2024-07-09 12:52:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 import os
 import re
-import json
 import streamlit as st
 import openai
 from openai import AssistantEventHandler
-from tools import TOOL_MAP
 from typing_extensions import override
 from dotenv import load_dotenv
-import streamlit_authenticator as stauth
 
 load_dotenv()
 
 # Configure the page
 st.set_page_config(
-    page_title="Assistants API UI",
+    page_title="Chat Interface",
     layout="wide",
     initial_sidebar_state="collapsed"
 )
@@ -85,145 +82,9 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-def str_to_bool(str_input):
-    if not isinstance(str_input, str):
-        return False
-    return str_input.lower() == "true"
-
-# Load environment variables
-azure_openai_endpoint = os.environ.get("AZURE_OPENAI_ENDPOINT")
-azure_openai_key = os.environ.get("AZURE_OPENAI_KEY")
-openai_api_key = os.environ.get("OPENAI_API_KEY")
-authentication_required = str_to_bool(os.environ.get("AUTHENTICATION_REQUIRED", False))
-assistant_id = os.environ.get("ASSISTANT_ID")
-assistant_title = os.environ.get("ASSISTANT_TITLE", "Assistants API UI")
-
-# Load authentication configuration
-if authentication_required:
-    if "credentials" in st.secrets:
-        authenticator = stauth.Authenticate(
-            st.secrets["credentials"].to_dict(),
-            st.secrets["cookie"]["name"],
-            st.secrets["cookie"]["key"],
-            st.secrets["cookie"]["expiry_days"],
-        )
-    else:
-        authenticator = None  # No authentication should be performed
-
-client = None
-if azure_openai_endpoint and azure_openai_key:
-    client = openai.AzureOpenAI(
-        api_key=azure_openai_key,
-        api_version="2024-02-15-preview",
-        azure_endpoint=azure_openai_endpoint,
-    )
-else:
-    client = openai.OpenAI(api_key=openai_api_key)
-
-class EventHandler(AssistantEventHandler):
-    @override
-    def on_event(self, event):
-        pass
-
-    @override
-    def on_text_created(self, text):
-        st.session_state.current_message = ""
-        with st.chat_message("Assistant"):
-            st.session_state.current_markdown = st.empty()
-
-    @override
-    def on_text_delta(self, delta, snapshot):
-        if snapshot.value:
-            text_value = re.sub(
-                r"\[(.*?)\]\s*\(\s*(.*?)\s*\)", "Download Link", snapshot.value
-            )
-            st.session_state.current_message = text_value
-            st.session_state.current_markdown.markdown(
-                st.session_state.current_message, True
-            )
-
-    @override
-    def on_text_done(self, text):
-        st.session_state.current_markdown.markdown(text.value, True)
-        st.session_state.chat_log.append({"name": "assistant", "msg": text.value})
-
-    @override
-    def on_tool_call_created(self, tool_call):
-        if tool_call.type == "code_interpreter":
-            st.session_state.current_tool_input = ""
-            with st.chat_message("Assistant"):
-                st.session_state.current_tool_input_markdown = st.empty()
-
-    @override
-    def on_tool_call_delta(self, delta, snapshot):
-        if 'current_tool_input_markdown' not in st.session_state:
-            with st.chat_message("Assistant"):
-                st.session_state.current_tool_input_markdown = st.empty()
-
-        if delta.type == "code_interpreter":
-            if delta.code_interpreter.input:
-                st.session_state.current_tool_input += delta.code_interpreter.input
-                input_code = f"### code interpreter\ninput:\n```python\n{st.session_state.current_tool_input}\n```"
-                st.session_state.current_tool_input_markdown.markdown(input_code, True)
-
-    @override
-    def on_tool_call_done(self, tool_call):
-        st.session_state.tool_calls.append(tool_call)
-        if tool_call.type == "code_interpreter":
-            if tool_call.id in [x.id for x in st.session_state.tool_calls]:
-                return
-            input_code = f"### code interpreter\ninput:\n```python\n{tool_call.code_interpreter.input}\n```"
-            st.session_state.current_tool_input_markdown.markdown(input_code, True)
-            st.session_state.chat_log.append({"name": "assistant", "msg": input_code})
-            st.session_state.current_tool_input_markdown = None
-            for output in tool_call.code_interpreter.outputs:
-                if output.type == "logs":
-                    output = f"### code interpreter\noutput:\n```\n{output.logs}\n```"
-                    with st.chat_message("Assistant"):
-                        st.markdown(output, True)
-                        st.session_state.chat_log.append(
-                            {"name": "assistant", "msg": output}
-                        )
-        elif (
-            tool_call.type == "function"
-            and self.current_run.status == "requires_action"
-        ):
-            with st.chat_message("Assistant"):
-                msg = f"### Function Calling: {tool_call.function.name}"
-                st.markdown(msg, True)
-                st.session_state.chat_log.append({"name": "assistant", "msg": msg})
-            tool_calls = self.current_run.required_action.submit_tool_outputs.tool_calls
-            tool_outputs = []
-            for submit_tool_call in tool_calls:
-                tool_function_name = submit_tool_call.function.name
-                tool_function_arguments = json.loads(
-                    submit_tool_call.function.arguments
-                )
-                tool_function_output = TOOL_MAP[tool_function_name](
-                    **tool_function_arguments
-                )
-                tool_outputs.append(
-                    {
-                        "tool_call_id": submit_tool_call.id,
-                        "output": tool_function_output,
-                    }
-                )
-
-            with client.beta.threads.runs.submit_tool_outputs_stream(
-                thread_id=st.session_state.thread.id,
-                run_id=self.current_run.id,
-                tool_outputs=tool_outputs,
-                event_handler=EventHandler(),
-            ) as stream:
-                stream.until_done()
-
+# Define the functions for handling chat messages
 def create_thread(content):
-    messages = [
-        {
-            "role": "user",
-            "content": content,
-        }
-    ]
+    messages = [{"role": "user", "content": content}]
     thread = client.beta.threads.create()
     return thread
 
@@ -268,25 +129,7 @@ if "in_progress" not in st.session_state:
 def disable_form():
     st.session_state.in_progress = True
 
-def login():
-    if st.session_state["authentication_status"] is False:
-        st.error("Username/password is incorrect")
-    elif st.session_state["authentication_status"] is None:
-        st.warning("Please enter your username and password")
-
 def main():
-    if (
-        authentication_required
-        and "credentials" in st.secrets
-        and authenticator is not None
-    ):
-        authenticator.login()
-        if not st.session_state["authentication_status"]:
-            login()
-            return
-        else:
-            authenticator.logout(location="main")
-
     st.markdown('<div class="chat-container">', unsafe_allow_html=True)
     st.markdown('<div class="chat-header"><h2>Chat</h2></div>', unsafe_allow_html=True)
     st.markdown('<div class="chat-messages">', unsafe_allow_html=True)
